39a329e8ccaee3aa60b3e3cf44aef1fa5d53ff1a
Eric Chang <ericjunchang@gmail.com>
2022-05-21 23:48:06 -0400

    Adds InventoryItem model

---
+++ inventory_item.py
@@ -0,0 +1,8 @@
+class InventoryItem:
+    def __init__(self, id, item, ref, quantity, archive, comment):
+        self.id = id
+        self.item = item
+        self.ref = ref
+        self.quantity = quantity
+        self.archive = archive
+        self.comment = comment
